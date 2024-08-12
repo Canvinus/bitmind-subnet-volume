e1a59dc939298c653a9e239832c88cbb730c63cb
Andrey Gruzdev <agruzdev1@gmail.com>
2024-08-12 18:03:07 +0200

    chore: update system deps install script

--- install_system_deps.sh
+++ install_system_deps.sh
@@ -2,6 +2,7 @@ sudo apt update -y
 sudo apt install python3-pip -y
 sudo apt install nano -y
 sudo apt install libgl1 -y
+sudo apt install nfs-common -y
 sudo apt install npm -y
 sudo npm install pm2@latest -g 
 
